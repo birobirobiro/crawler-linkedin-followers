9e94bc1391c96d71980fdb797131f4711055d9d7
João Inácio Neto <birobirobiro.dev@gmail.com>
2023-02-13 23:37:29 -0300

    v1

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const ms = require('ms');
+const app = express();
+const puppeteer = require('puppeteer');
+
+function removeFollowers(str) {
+  return str.replace(/\s*followers\s*/, '');
+}
+
+let browser;
+
+app.get('/', async (req, res) => {
+  try {
+    if (!browser) {
+      browser = await puppeteer.launch({
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+      });
+    }
+
+    const page = await browser.newPage();
+    await page.setDefaultNavigationTimeout(0);
+
+    await page.goto('https://www.linkedin.com/in/birobirobiro/?original_referer=', {
+      waitUntil: 'load',
+      setTimeout: ms('0s'),
+    });
+
+
+    await page.waitForTimeout('1ms');
+
+    await page.waitForSelector('span[class^="top-card__subline-item"');
+    const followersCount = await page.$eval('span[class^="top-card__subline-item"', (el) => el.innerText);
+
+    res.send({
+      "followers": removeFollowers(followersCount),
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({
+      error: err.message,
+    });
+  }
+});
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
